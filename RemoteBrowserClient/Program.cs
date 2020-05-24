9522e1c4c34ece7a1c44032d438800e8890433a7
Gustavo Arend Dill <gustavoarenddill42@gmail.com>
2020-05-23 22:21:32 -0300

    Project create - commands ready, must add some stuff

---
+++ RemoteBrowserClient/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+
+namespace RemoteBrowserClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var loginCode = "RemoteBrowser#CODE#";
+            RemoteBrowserClient client = new RemoteBrowserClient("192.168.0.106", 4782);
+            Console.Write("Connecting to server...");
+            try
+            {
+                client.Connect();
+                client.SendString(loginCode);
+                Thread.Sleep(1000);
+                string rec = "";
+                client.ClientSocket.ReceiveTimeout = 3000;
+                try { rec = client.ReceiveString(); } catch { }
+                client.ClientSocket.ReceiveTimeout = 0;
+                if (rec == "Code rejected")
+                    try { client.Disconnect(); Console.WriteLine("\tCode rejected"); Thread.Sleep(2000); Environment.Exit(1); } catch { Console.WriteLine("\tCode rejected"); Thread.Sleep(2000); Environment.Exit(1); }
+            }
+            catch
+            {
+
+            }
+            Thread.Sleep(1250);
+            Console.WriteLine(client.Connected ? "\tConnected" : "\tFailed");
+            Thread.Sleep(1250);
+            if (!client.Connected)
+                Environment.Exit(0);
+            L:
+            Console.Write(">");
+            var d = Console.ReadLine();
+            if (d == "exit")
+                Environment.Exit(0);
+            var list = client.ListDirectory(d);
+            Console.WriteLine("Files");
+            foreach (var f in list.Files)
+                Console.WriteLine("\t" + f);
+            Console.WriteLine("Directories");
+            foreach (var dc in list.Directories)
+                Console.WriteLine("\t" + dc);
+            goto L;
+        }
+    }
+}
---
+++ RemoteBrowserClient/RemoteBrowserClient.cs
@@ -0,0 +1,48 @@
+﻿using CSharpExtendedCommands.Web.Communication;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace RemoteBrowserClient
+{
+    public class RemoteBrowserClient : TCPClient
+    {
+        public class DirectoryList
+        {
+            public DirectoryList(string[] files, string[] directories)
+            {
+                Files = files;
+                Directories = directories;
+            }
+
+            public string[] Files { get; }
+            public string[] Directories { get; }
+        }
+        public RemoteBrowserClient(string ip, ushort port)
+        {
+            Ip = IPAddress.Parse(ip);
+            Port = port;
+        }
+        public void SendCommand(string command, string args = null)
+        {
+            var s = command.ToUpper();
+            s += args != null ? ": \"" + args + "\"" : "";
+            SendPackage(new TcpPackage(s));
+        }
+        public DirectoryList ListDirectory(string dir)
+        {
+            SendCommand("list-directory", dir);
+            var package = ReceivePackage().ToString();
+            var filedata = package.Substring(0, package.IndexOf("\nDirectories:\n"));
+            var directoryData = package.Substring(filedata.Length + 1);
+            List<string> mem = new List<string>();
+            foreach (Match m in Regex.Matches(filedata, @"\t(.+);"))
+                mem.Add(m.Groups[1].Value);
+            string[] files = mem.ToArray();
+            mem.Clear();
+            foreach (Match m in Regex.Matches(directoryData, @"\t(.+);"))
+                mem.Add(m.Groups[1].Value);
+            return new DirectoryList(files, mem.ToArray());
+        }
+    }
+}
